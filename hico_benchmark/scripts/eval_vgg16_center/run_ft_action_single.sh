679efc57f976ab88551a876bce5820c7669b7fae
Yu-Wei Chao <ywchao@umich.edu>
2016-05-23 05:03:25 -0400

    hico_benchmark eval vgg16

---
+++ hico_benchmark/scripts/eval_vgg16_center/run_ft_action_single.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p logs/eval_vgg16_center
+
+LOG="logs/eval_vgg16_center/ft_action_single.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time matlab -r "feat_type = 'feat_vgg16_center-ft_action_single'; train_svm_vo; eval_default_run; eval_ko_run;"
